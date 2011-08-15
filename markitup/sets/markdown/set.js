f5bcf1fabf0abb4488835741ca9fde75de7dedf4
Will Nielsen <will@nielsendigital.com>
2011-08-15 15:03:28 -0500

    updated set.js and style.css for markdown

--- markitup/sets/markdown/set.js
+++ markitup/sets/markdown/set.js
@@ -14,28 +14,28 @@ mySettings = {
 	previewParserPath:	'',
 	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
 	markupSet: [
-		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
-		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
-		{separator:'---------------' },		
+        { name:'Headings', dropMenu: [
+            {name:'Heading', key:'1', openWith:'### ', placeHolder:'Your title here...' },
+            {name:'Child Heading', key:'2', openWith:'#### ', placeHolder:'Your title here...' },
+            {name:'Grandchild Heading', key:'3', openWith:'##### ', placeHolder:'Your title here...' },
+          ]
+        }, 
+	
 		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
 		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
-		{separator:'---------------' },
-		{name:'Bulleted List', openWith:'- ' },
+
+        {name:'Bulleted List', openWith:'- ' },
 		{name:'Numeric List', openWith:function(markItUp) {
 			return markItUp.line+'. ';
 		}},
-		{separator:'---------------' },
-		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
+
+        {name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
 		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
-		{separator:'---------------'},	
-		{name:'Quotes', openWith:'> '},
+
+        {name:'Quotes', openWith:'> '},
 		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
-		{separator:'---------------'},
-		{name:'Preview', call:'preview', className:"preview"}
+
+        {name:'Preview', call:'preview', className:"preview"}
 	]
 }
 
